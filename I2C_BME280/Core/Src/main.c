4a90a66fd074191ee0a488127b0f27ed856808bc
Il Seok Oh <iloh@microsoft.com>
2021-11-22 20:23:16 +0900

    initial commit

---
+++ I2C_BME280/Core/Src/main.c
@@ -0,0 +1,542 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include <stdio.h>
+#include "bme280.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+void user_delay_ms(uint32_t period, void *intf_ptr);
+int8_t user_i2c_write(uint8_t reg_addr, const uint8_t *data, uint32_t len, void *intf_ptr);
+int8_t user_i2c_read(uint8_t reg_addr, uint8_t *data, uint32_t len, void *intf_ptr);
+int8_t stream_sensor_data_forced_mode(struct bme280_dev *dev);
+void print_sensor_data(struct bme280_data *comp_data);
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+I2C_HandleTypeDef hi2c1;
+
+UART_HandleTypeDef huart2;
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_USART2_UART_Init(void);
+static void MX_I2C1_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+#ifdef __GNUC__
+#define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
+#else
+#define PUTCHAR_PROTOTYPE int fputc(int ch,FILE *f)
+#endif
+
+PUTCHAR_PROTOTYPE
+{
+	if (ch == '\n')
+		HAL_UART_Transmit (&huart2, (uint8_t*) "\r", 1, 0xFFFF);
+	HAL_UART_Transmit (&huart2, (uint8_t*) &ch, 1, 0xFFFF);
+	return ch;
+}
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USART2_UART_Init();
+  MX_I2C1_Init();
+  /* USER CODE BEGIN 2 */
+
+  printf ("\nBME280\n");
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+
+  // chip id read
+#if 0
+  uint8_t buf[1];
+  uint8_t buf2[1];
+  HAL_StatusTypeDef error;
+
+  // read register
+  buf[0] = BMP280_CHIP_ID_ADDR;
+  error = HAL_I2C_Master_Transmit (&hi2c1, BMP280_I2C_ADDR_PRIM << 1, buf, 1, 100);
+  if (error != HAL_OK) printf ("error \n");
+
+  HAL_Delay(100);
+
+  error = HAL_I2C_Master_Receive(&hi2c1, BMP280_I2C_ADDR_PRIM << 1, buf2, 1, 100);
+  if (error != HAL_OK) printf ("error receive \n");
+
+  printf ("Chip ID: %X\n", buf2[0]);
+
+#else
+  struct bme280_dev dev;
+  int8_t rslt = BME280_OK;
+  uint8_t dev_addr = BME280_I2C_ADDR_PRIM;
+
+  dev.intf_ptr = &dev_addr;
+  dev.intf = BME280_I2C_INTF;
+  dev.read = user_i2c_read;
+  dev.write = user_i2c_write;
+  dev.delay_us = user_delay_ms;
+
+  rslt = bme280_init(&dev);
+
+  if (rslt == BME280_OK)
+  {
+	  printf("Chip ID: 0x%X\n", dev.chip_id);
+	  printf("Cal Data dig_h1: 0x%X\n", dev.calib_data.dig_h1);
+	  printf("Cal Data dig_h2: %d\n", dev.calib_data.dig_h2);
+  }
+  else
+  {
+	  printf("Init Error");
+  }
+
+  rslt = stream_sensor_data_forced_mode(&dev);
+
+
+#endif
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI_DIV2;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL16;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief I2C1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C1_Init(void)
+{
+
+  /* USER CODE BEGIN I2C1_Init 0 */
+
+  /* USER CODE END I2C1_Init 0 */
+
+  /* USER CODE BEGIN I2C1_Init 1 */
+
+  /* USER CODE END I2C1_Init 1 */
+  hi2c1.Instance = I2C1;
+  hi2c1.Init.ClockSpeed = 400000;
+  hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
+  hi2c1.Init.OwnAddress1 = 0;
+  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c1.Init.OwnAddress2 = 0;
+  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C1_Init 2 */
+
+  /* USER CODE END I2C1_Init 2 */
+
+}
+
+/**
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART2_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART2_Init 0 */
+
+  /* USER CODE END USART2_Init 0 */
+
+  /* USER CODE BEGIN USART2_Init 1 */
+
+  /* USER CODE END USART2_Init 1 */
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART2_Init 2 */
+
+  /* USER CODE END USART2_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : LD2_Pin */
+  GPIO_InitStruct.Pin = LD2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
+
+}
+
+/* USER CODE BEGIN 4 */
+/*!
+ *  @brief Function for writing the sensor's registers through I2C bus.
+ *
+ *  @param[in] reg_addr       : Register address.
+ *  @param[in] data           : Pointer to the data buffer whose value is to be written.
+ *  @param[in] len            : No of bytes to write.
+ *  @param[in, out] intf_ptr  : Void pointer that can enable the linking of descriptors
+ *                                  for interface related call backs
+ *
+ *  @return Status of execution
+ *
+ *  @retval BME280_OK -> Success
+ *  @retval BME280_E_COMM_FAIL -> Communication failure.
+ *
+ */
+int8_t user_i2c_write(uint8_t reg_addr, const uint8_t *data, uint32_t len, void *intf_ptr)
+{
+
+    /* Implement the I2C write routine according to the target machine. */
+	HAL_StatusTypeDef error;
+
+	//printf("inf_ptr: %d", intf_ptr);
+
+	uint8_t id = BME280_I2C_ADDR_PRIM;
+
+
+	error = HAL_I2C_Master_Transmit (&hi2c1, id << 1, &reg_addr, 1, 100);
+	if (error != HAL_OK)
+	{
+		return BME280_E_COMM_FAIL;
+	}
+
+	error = HAL_I2C_Master_Transmit (&hi2c1, id << 1, data, len, 100);
+		if (error != HAL_OK)
+		{
+			return BME280_E_COMM_FAIL;
+		}
+
+	return BME280_OK;
+}
+
+/*!
+ *  @brief Function for reading the sensor's registers through I2C bus.
+ *
+ *  @param[in] reg_addr       : Register address.
+ *  @param[out] data          : Pointer to the data buffer to store the read data.
+ *  @param[in] len            : No of bytes to read.
+ *  @param[in, out] intf_ptr  : Void pointer that can enable the linking of descriptors
+ *                                  for interface related call backs.
+ *
+ *  @return Status of execution
+ *
+ *  @retval 0 -> Success
+ *  @retval > 0 -> Failure Info
+ *
+ */
+int8_t user_i2c_read(uint8_t reg_addr, uint8_t *data, uint32_t len, void *intf_ptr)
+{
+
+    /* Implement the I2C read routine according to the target machine. */
+	HAL_StatusTypeDef error;
+
+	//printf("inf_ptr: %X", intf_ptr);
+
+	uint8_t id = BME280_I2C_ADDR_PRIM;
+
+	error = HAL_I2C_Master_Transmit (&hi2c1, id << 1, &reg_addr, 1, 100);
+	if (error != HAL_OK)
+	{
+		return BME280_E_COMM_FAIL;
+	}
+
+	error = HAL_I2C_Master_Receive(&hi2c1, id << 1 | 0x01, data, len, 100);
+	if (error != HAL_OK)
+	{
+		return BME280_E_COMM_FAIL;
+	}
+    return BME280_OK;
+}
+
+/*!
+ *  @brief Function that creates a mandatory delay required in some of the APIs.
+ *
+ * @param[in] period              : Delay in microseconds.
+ * @param[in, out] intf_ptr       : Void pointer that can enable the linking of descriptors
+ *                                  for interface related call backs
+ *  @return void.
+ *
+ */
+void user_delay_ms(uint32_t period, void *intf_ptr)
+{
+	HAL_Delay(period);
+}
+
+
+/*!
+ * @brief This API reads the sensor temperature, pressure and humidity data in forced mode.
+ */
+int8_t stream_sensor_data_forced_mode(struct bme280_dev *dev)
+{
+    int8_t rslt;
+    uint8_t settings_sel;
+    struct bme280_data comp_data;
+
+    /* Recommended mode of operation: Indoor navigation */
+    dev->settings.osr_h = BME280_OVERSAMPLING_1X;
+    dev->settings.osr_p = BME280_OVERSAMPLING_16X;
+    dev->settings.osr_t = BME280_OVERSAMPLING_2X;
+    dev->settings.filter = BME280_FILTER_COEFF_16;
+
+    settings_sel = BME280_OSR_PRESS_SEL | BME280_OSR_TEMP_SEL | BME280_OSR_HUM_SEL | BME280_FILTER_SEL;
+
+    rslt = bme280_set_sensor_settings(settings_sel, dev);
+    if (rslt != BME280_OK)
+    {
+        printf("Failed to set sensor settings (code %d).", rslt);
+
+        return rslt;
+    }
+
+    printf("Temperature, Pressure, Humidity\n");
+
+    /* Continuously stream sensor data */
+    while (1)
+    {
+        rslt = bme280_set_sensor_mode(BME280_FORCED_MODE, dev);
+        if (rslt != BME280_OK)
+        {
+            fprintf(stderr, "Failed to set sensor mode (code %+d).", rslt);
+            break;
+        }
+
+        /* Wait for the measurement to complete and print data @25Hz */
+        dev->delay_us(40000, dev->intf_ptr);
+        rslt = bme280_get_sensor_data(BME280_ALL, &comp_data, dev);
+        if (rslt != BME280_OK)
+        {
+            fprintf(stderr, "Failed to get sensor data (code %+d).", rslt);
+            break;
+        }
+
+        print_sensor_data(&comp_data);
+        dev->delay_us(1000000, dev->intf_ptr);
+    }
+
+    return rslt;
+}
+
+/*!
+ * @brief This API used to print the sensor temperature, pressure and humidity data.
+ */
+void print_sensor_data(struct bme280_data *comp_data)
+{
+    float temp, press, hum;
+
+	#ifdef BME280_FLOAT_ENABLE
+		temp = comp_data->temperature;
+		press = 0.01 * comp_data->pressure;
+		hum = comp_data->humidity;
+	#else
+	#ifdef BME280_64BIT_ENABLE
+		temp = 0.01f * comp_data->temperature;
+		press = 0.0001f * comp_data->pressure;
+		hum = 1.0f / 1024.0f * comp_data->humidity;
+	#else
+		temp = 0.01f * comp_data->temperature;
+		press = 0.01f * comp_data->pressure;
+		hum = 1.0f / 1024.0f * comp_data->humidity;
+	#endif
+	#endif
+    printf("%0.2lf deg C, %0.2lf hPa, %0.2lf%%\n", temp, press, hum);
+}
+
+/*void print_rslt(const char api_name[], int8_t rslt)
+{
+    if (rslt != BMP280_OK)
+    {
+        printf("%s\t", api_name);
+        if (rslt == BMP280_E_NULL_PTR)
+        {
+            printf("Error [%d] : Null pointer error\r\n", rslt);
+        }
+        else if (rslt == BMP280_E_COMM_FAIL)
+        {
+            printf("Error [%d] : Bus communication failed\r\n", rslt);
+        }
+        else if (rslt == BMP280_E_IMPLAUS_TEMP)
+        {
+            printf("Error [%d] : Invalid Temperature\r\n", rslt);
+        }
+        else if (rslt == BMP280_E_DEV_NOT_FOUND)
+        {
+            printf("Error [%d] : Device not found\r\n", rslt);
+        }
+        else
+        {
+            // For more error codes refer "*_defs.h"
+            printf("Error [%d] : Unknown error code\r\n", rslt);
+        }
+    }
+}*/
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
